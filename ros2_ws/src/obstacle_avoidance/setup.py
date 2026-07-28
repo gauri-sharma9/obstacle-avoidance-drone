4b25dc458c20cca8b8f8d602e26284dd7a8e1d1b
Gauri Sharma <gaurisharma9104@gmail.com>
2026-07-28 08:26:05 +0530

    Fix ROS2 package configuration and restore build

---
+++ ros2_ws/src/obstacle_avoidance/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+package_name = 'obstacle_avoidance'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name],
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml'],
+        ),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Gauri',
+    maintainer_email='your_email@example.com',
+    description='Obstacle Avoidance Drone Framework',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'obstacle_detector = obstacle_avoidance.obstacle_detector:main',
+        ],
+    },
+)
